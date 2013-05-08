96e49e154c374870efd37ec97054d8a45ba892f5
Thomas Levine <occurrence@thomaslevine.com>
2013-05-08 12:31:59 -0400

    apikey

--- stormpath.py
+++ stormpath.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python2
 
-import stormpath
+from stormpath.client import ClientBuilder
+
+api_key_file = 'apikey.yml'
+client = ClientBuilder().set_api_key_file_location(api_key_file).build()`
